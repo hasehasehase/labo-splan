3134ad337bf9bff7d454f5942da6650278093f1d
Hase Hase <kecz0r@gmail.com>
2017-02-20 19:23:41 +0000

    fix date handling in mailer, fix seeds and dev-index-view

--- app/mailers/shift_mailer.rb
+++ app/mailers/shift_mailer.rb
@@ -5,28 +5,33 @@ default from: 'max.haselmayer@gmail.com'
   #
   #   en.shift_mailer.confirm_mail.subject
   #
-  def confirm_mail
-      @user_email = ENV["user_email"]
-      mail(to: @user_email, subject: 'Arbeiten.', body: 'Leider :(')
-  end
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.shift_mailer.deny_mail.subject
-  #
-  def deny_mail
-    @user_email = ENV["user_email"]
-      mail(to: @user_email, subject: 'Kein Arbeiten.', body: 'Yay! :)')
-  end
-  
-  def request_mail(uid)
-    @shift = Shift.find_by unique_identifier: (uid)
-    @name = @shift.name
-    @date = Date.strptime( @shift.date).strftime('%d.%m.%Y')
-    @time = @shift.time
-    @op_email = ENV["operator_email"]
-    @uid = uid
-    mail(to: @op_email, subject: "#{ @shift.name } am #{ @date }.")
-  end
+	def confirm_mail
+		@user_email = ENV["user_email"]
+		mail(to: @user_email, subject: 'Arbeiten.', body: 'Leider :(')
+	end
+	
+# Subject can be set in your I18n file at config/locales/en.yml
+# with the following lookup:
+#
+#   en.shift_mailer.deny_mail.subject
+#
+	def deny_mail
+		@user_email = ENV["user_email"]
+		mail(to: @user_email, subject: 'Kein Arbeiten.', body: 'Yay! :)')
+	end
+	
+	def request_mail(uid)
+		@shift = Shift.find_by unique_identifier: (uid)
+		@name = @shift.name
+		begin
+			@date = Date.strptime(@shift.date).strftime('%d.%m.%Y')
+		rescue ArgumentError
+			puts "invalid Date in Shift#{ @shift.id }. Mail sent anyways."
+			@date = @shift.date
+		end
+		@time = @shift.time
+		@op_email = ENV["operator_email"]
+		@uid = uid
+		mail(to: @op_email, subject: "#{ @shift.name } am #{ @date }.")
+	end
 end
